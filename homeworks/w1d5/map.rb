0df5e8878264d1f03fd4d9b7572ad8d023b9fa8a
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-17 21:18:23 -0800

    Add assign, lookup, and initialize methods of map ADT nested array implementation

--- homeworks/w1d5/map.rb
+++ homeworks/w1d5/map.rb
@@ -1,14 +1,17 @@
 class Map
-  def initialize
-
+  def initialize(my_map = [])
+    @my_map = my_map
   end
 
   def assign(key, value)
-
+    my_map << [key, value] if lookup(key).nil?
   end
 
   def lookup(key)
+    return nil if my_map.empty?
 
+    my_map.each { |entry| return entry.last if entry.first == key }
+    nil
   end
 
   def remove(key)
@@ -18,4 +21,8 @@ class Map
   def show
 
   end
+
+  private
+
+  attr_accessor :my_map
 end
