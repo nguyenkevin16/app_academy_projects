d707003e2950457a9b259d5ebc4afb28323b0ba2
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-17 21:01:51 -0800

    Add basic add, remove, and show methods for simple array stack

--- homeworks/w1d5/stack.rb
+++ homeworks/w1d5/stack.rb
@@ -6,17 +6,20 @@ class Stack
 
   def add(el)
     # adds an element to the stack
+    stack.push(el)
   end
 
   def remove
     # removes one element from the stack
+    stack.pop
   end
 
   def show
     # return a copy of the stack
+    stack
   end
 
   private
 
-  attr_reader :stack
+  attr_accessor :stack
 end
