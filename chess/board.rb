c68e05010c3dc644b688ffbe2dcd4c3fb873ff26
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-21 14:11:23 -0800

    Add basic framework for board class

---
+++ chess/board.rb
@@ -0,0 +1,49 @@
+require_relative 'piece'
+
+class Board
+  attr_reader :board
+
+  def initialize
+    @board = Array.new(8) { Array.new }
+    populate
+  end
+
+  def populate
+    0.upto(7) do |idx|
+      piece_row = []
+      if idx >= 2 && idx < 6
+        8.times { piece_row << nil }
+      else
+        8.times { piece_row << Piece.new }
+      end
+      @board[idx] += piece_row
+    end
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise if self[start_pos].nil? ||
+      end_pos.any? { |coord| coord < 0 || coord > 7 }
+    rescue
+      puts "Invalid position."
+
+    self[end_pos] = self[start_pos]
+    self[start_pos] = nil
+  end
+
+  def [](pos)
+    row, col = pos
+    @board[row][col]
+  end
+
+  def []=(pos, mark)
+    row, col = pos
+    @board[row][col] = mark
+  end
+end
+
+if __FILE__ == $0
+  b = Board.new
+  puts b.board
+  b.move_piece([0,0], [-1, 5])
+  puts b.board
+end
