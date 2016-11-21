a261042c300955bed1f2dee7c3d99eb7c3a2338b
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-20 16:09:37 -0800

    Implement solutions for w1d1 enumerables as part of assessment 1 practice

--- projects/w1d1/enumerables/a01_practice.rb
+++ projects/w1d1/enumerables/a01_practice.rb
@@ -27,6 +27,62 @@ class Array
   end
 
   def my_flatten
-
+    new_arr = []
+    self.each do |el|
+      new_arr += (el.is_a?(Array) ? el.my_flatten : [el])
+    end
+    new_arr
   end
+
+  def my_zip(*zip_files)
+    zipped_arr = []
+
+    self.each_index do |idx|
+      sub_arr = [self[idx]]
+
+      zip_files.each do |zip_file|
+        sub_arr << (zip_file.nil? ? nil : zip_file[idx])
+      end
+
+      zipped_arr << sub_arr
+    end
+
+    zipped_arr
+  end
+
+  def my_rotate(shift = 1)
+    arr = self.dup
+    num = (shift > 0 ? shift : shift * -1)
+
+    num.times do
+      if shift > 0
+        arr << arr.shift
+      else
+        arr.unshift(arr.pop)
+      end
+    end
+
+    arr
+  end
+
+  def my_join(seperator = "")
+    str = ""
+    self.each { |el| str << el + seperator }
+    str
+  end
+
+  def my_reverse
+    reversed_arr = []
+
+    (self.length - 1).downto(0).each do |idx|
+      reversed_arr << self[idx]
+    end
+    
+    reversed_arr
+  end
+
+end
+
+if __FILE__ == $0
+
 end
