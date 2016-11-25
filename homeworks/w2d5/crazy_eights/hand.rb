afb6ce10bffafa39376d94dcae89c1195bd83000
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-24 21:04:43 -0800

    Add files for crazy eights practice

--- homeworks/w2d5/crazy_eights/hand.rb
+++ homeworks/w2d5/crazy_eights/hand.rb
@@ -4,7 +4,7 @@ class Hand
   # object. This is in contrast to the `#initialize` method that
   # expects an `Array` of cards to hold.
   def self.deal_from(deck)
-    cards = deck.take(2)
+    cards = deck.take(5)
 
     Hand.new(cards)
   end
@@ -16,38 +16,19 @@ class Hand
   end
 
   def points
-    points = 0
 
-    @cards.each do |card|
-      if card.value == :ace # is an ace
-        if points + 11 > 21
-          points += 1
-        else
-          points += 11
-        end
-      else
-        points += card.blackjack_value
-      end
-    end
-
-    points
   end
 
   def busted?
-    return true if points > 21
-    false
+
   end
 
   def hit(deck)
-    raise "already busted" if busted?
-    @cards += deck.take(1)
+
   end
 
   def beats?(other_hand)
-    return false if busted?
-    return true if other_hand.busted?
-    return true if other_hand.points < self.points
-    return false if other_hand.points >= self.points
+
   end
 
   def return_cards(deck)
