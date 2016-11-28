99b4e70b57e35ae2974cd1118fb726526939c735
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-27 20:40:02 -0800

    Add redo of simon as assessment 02 practice

--- homeworks/w2d1/simon_redo/lib/simon.rb
+++ homeworks/w2d1/simon_redo/lib/simon.rb
@@ -4,19 +4,33 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
 
+    if @game_over == false
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    seq
   end
 
   def require_sequence
@@ -24,7 +38,7 @@ class Simon
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +50,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
