a22c3d927aede48476d9c61995ebe143d9b1d7d8
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-28 19:27:20 -0800

    Add framework for object relational mapping homework

---
+++ homeworks/w3d2/orm/plays_skeleton/plays.rb
@@ -0,0 +1,62 @@
+require 'sqlite3'
+require 'singleton'
+
+class PlayDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('plays.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Play
+  attr_accessor :title, :year, :playwright_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @year = options['year']
+    @playwright_id = options['playwright_id']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id)
+      INSERT INTO
+        plays (title, year, playwright_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @title, @year, @playwright_id, @id)
+      UPDATE
+        plays
+      SET
+        title = ?, year = ?, playwright_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
+
+class Playwright
+  # Playwright::all
+  # Playwright::find_by_name(name)
+  # Playwright#new
+  # Playwright#create
+  # Playwright#update
+  # Playwright#get_plays (returns all plays written by playwright)
+  # Play::find_by_title(title)
+  # Play::find_by_playwright(name) (returns all plays written by playwright)
+end
