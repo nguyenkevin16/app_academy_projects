4c0764cc91d155a6a7b84575dcbf1548923bed68
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-30 14:55:10 -0800

    Implement visit join table and counter methods for shortened url

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -7,6 +7,16 @@ class ShortenedUrl < ActiveRecord::Base
     foreign_key: :user_id,
     class_name: :User
 
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :Visit
+
+  has_many :visitors,
+    -> { distinct },
+    through: :visits,
+    source: :visitor
+
   def self.random_code
     random_code = SecureRandom.urlsafe_base64
     until !ShortenedUrl.exists?(short_url: random_code)
@@ -18,5 +28,17 @@ class ShortenedUrl < ActiveRecord::Base
   def self.create_for_user_and_long_url!(user, long_url)
     short_url = ShortenedUrl.random_code
     ShortenedUrl.create!(user_id: user.id, long_url: long_url, short_url: short_url)
+  end
+
+  def num_clicks
+    self.visits.length
+  end
+
+  def num_uniques
+    self.visits.select(:user_id).distinct.count
+  end
+
+  def num_recent_uniques
+    self.visits.select(:user_id).distinct.where("created_at > ?", 1.hour.ago).count
   end
 end
--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130200701) do
+ActiveRecord::Schema.define(version: 20161130221324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,15 @@ ActiveRecord::Schema.define(version: 20161130200701) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
+
+  create_table "visits", force: :cascade do |t|
+    t.integer  "user_id",          null: false
+    t.integer  "shortened_url_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "visits", ["shortened_url_id"], name: "index_visits_on_shortened_url_id", using: :btree
+  add_index "visits", ["user_id"], name: "index_visits_on_user_id", using: :btree
 
 end
