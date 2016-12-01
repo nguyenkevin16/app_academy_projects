61b3a3569bbbc375375df0533ab5143ccbe8fd3a
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-30 17:45:07 -0800

    add most popular websites by topic feature

--- URLShortener/app/models/tag_topic.rb
+++ URLShortener/app/models/tag_topic.rb
@@ -10,5 +10,26 @@ class TagTopic < ActiveRecord::Base
     through: :taggings,
     source: :url
 
-  has_many 
+  has_many :visits,
+    through: :urls,
+    source: :visits
+
+  def most_popular_urls(n)
+    num_urls = urls.count
+    n = num_urls if n > num_urls
+
+    # order the urls by # of likes DESC
+    url_visit_counts = Hash.new
+
+    # urls = self.urls.include(:visits)
+    urls.each do |url|
+      url_visit_counts[url] = url.num_clicks
+    end
+
+    websites = url_visit_counts.sort_by { |url, visits| visits }.pop(n)
+    websites.map { |pair| pair[0] }
+  end
 end
+
+
+# n + 1 query
