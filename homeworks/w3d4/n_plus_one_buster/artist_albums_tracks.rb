4602fd9770a5a8dd270a4edc42bd0c16698e3c78
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-11-30 19:21:41 -0800

    Add skeleton files for n_plus_one_buster

---
+++ homeworks/w3d4/n_plus_one_buster/artist_albums_tracks.rb
@@ -0,0 +1,54 @@
+# Count all of the tracks on each album by a given artist.
+
+# app/models/track.rb
+class Track
+  belongs_to(
+    :album,
+    class_name: "Album",
+    foreign_key: :album_id,
+    primary_key: :id
+  )
+end
+
+
+# app/models/album.rb
+class Album
+  belongs_to(
+    :artist,
+    class_name: "Artist",
+    foreign_key: :artist_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :tracks,
+    class_name: "Track",
+    foreign_key: :album_id,
+    primary_key: :id
+  )
+end
+
+
+# app/models/artist.rb
+class Artist
+  has_many(
+    :albums,
+    class_name: "Album",
+    foreign_key: :artist_id,
+    primary_key: :id
+  )
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.name] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    # TODO: your code here
+  end
+end
