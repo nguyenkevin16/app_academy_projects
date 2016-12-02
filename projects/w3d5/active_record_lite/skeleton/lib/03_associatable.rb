e7a8d05c2709f6de904136c5f9583d3d46aacb8f
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-02 12:30:26 -0800

    implement solution for belongs_to association in 03_associatable

--- projects/w3d5/active_record_lite/skeleton/lib/03_associatable.rb
+++ projects/w3d5/active_record_lite/skeleton/lib/03_associatable.rb
@@ -10,30 +10,45 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    options.each { |attr_name, value| send("#{attr_name}=", value) }
+
+    @foreign_key ||= "#{name}_id".to_sym
+    @primary_key ||= "id".to_sym
+    @class_name ||= name.to_s.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    options.each { |attr_name, value| send("#{attr_name}=", value) }
+
+    @foreign_key ||= "#{self_class_name.downcase}_id".to_sym
+    @primary_key ||= "id".to_sym
+    @class_name ||= name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      model_class = options.model_class
+      foreign_key = self.send(options.foreign_key)
+
+      model_class.where(id: foreign_key).first
+    end
   end
 
   def has_many(name, options = {})
@@ -47,4 +62,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
