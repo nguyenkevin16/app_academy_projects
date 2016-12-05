5e8cc39c5743e7143f2ec09a465917886ceb936c
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-05 13:57:28 -0800

    Add create_user and delete_user methods to user controller script

--- projects/w4d1/FirstRoutesAndControllers/bin/my_script.rb
+++ projects/w4d1/FirstRoutesAndControllers/bin/my_script.rb
@@ -13,6 +13,36 @@ def index_users
   puts RestClient.get(url)
 end
 
+def create_user(name, email)
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+
+  puts RestClient.post(
+    url,
+    { user: { name: name, email: email } }
+  )
+end
+
+def delete_user(name, email)
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users'
+  ).to_s
+
+  puts RestClient.delete(
+    # url + '/4'
+  )
+end
+
 if __FILE__ == $0
   index_users
+  # create_user("Gizmo", "gizmo@gizmo.gizmo")
+  # delete_user("Gizmo", "gizmo@gizmo.gizmo")
+  # index_users
 end
