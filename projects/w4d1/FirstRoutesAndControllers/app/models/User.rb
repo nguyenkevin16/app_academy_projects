f4ca602d043d8d30d77a7ed2bb130b72257c4463
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-05 14:34:22 -0800

    Add contact and contact_shares models

--- projects/w4d1/FirstRoutesAndControllers/app/models/User.rb
+++ projects/w4d1/FirstRoutesAndControllers/app/models/User.rb
@@ -3,12 +3,13 @@
 # Table name: users
 #
 #  id         :integer          not null, primary key
-#  name       :string           not null
-#  email      :string           not null
+#  username   :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
+
+  has_many :contacts
 end
---
+++ projects/w4d1/FirstRoutesAndControllers/app/models/contact.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Contact < ActiveRecord::Base
+  validates :name, :email, :user_id, presence: true
+  validates :name, uniqueness: { scope: :email }
+
+  belongs_to :owner
+end
