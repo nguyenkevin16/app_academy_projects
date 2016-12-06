73a2bd332a1bf659d1e359e33054ebb40c42198b
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-06 12:07:58 -0800

    add edit and new forms as well as controller actions

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -1,4 +1,5 @@
 class CatsController < ApplicationController
+
   def index
     @cats = Cat.all
     render :index
@@ -8,4 +9,41 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :show
   end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save!
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    if @cat
+      render :edit
+    else
+      redirect_to new_cat_url
+    end
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def cat_params
+      params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
+    end
 end
