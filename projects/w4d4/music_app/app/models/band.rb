7d16adcc002558950406ee271a64fa4201dfaa27
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-08 10:29:36 -0800

    Add album model and table

--- projects/w4d4/music_app/app/models/band.rb
+++ projects/w4d4/music_app/app/models/band.rb
@@ -10,4 +10,6 @@
 
 class Band < ActiveRecord::Base
   validates :name, presence: true
+
+  has_many :albums
 end
--- projects/w4d4/music_app/db/schema.rb
+++ projects/w4d4/music_app/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208182213) do
+ActiveRecord::Schema.define(version: 20161208182530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name",              null: false
+    t.string   "recorded_location", null: false
+    t.integer  "band_id",           null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "albums", ["band_id"], name: "index_albums_on_band_id", using: :btree
+  add_index "albums", ["name"], name: "index_albums_on_name", using: :btree
 
   create_table "bands", force: :cascade do |t|
     t.string   "name",       null: false
