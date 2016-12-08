d9b990d90e39eb57e508fb6cad9502aafe3c9085
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-08 13:47:27 -0800

    Add views for albums and finish albums controller

---
+++ projects/w4d4/music_app/app/controllers/albums_controller.rb
@@ -0,0 +1,54 @@
+class AlbumsController < ApplicationController
+  def show
+    @album = Album.find(params[:id])
+    render :show
+  end
+
+  def new
+    @bands = Band.all
+    @album = Album.new
+    render :new
+  end
+
+  def create
+    @album = Album.new(album_params)
+
+    if @album.save!
+      # redirect_to album_url(@album)
+      redirect_to album_url(@album)
+    else
+      # flash.now error
+      redirect_to new_album_url
+    end
+  end
+
+  def edit
+    @bands = Band.all
+    @album = Album.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update_attributes(album_params)
+      redirect_to album_url(@album)
+    else
+      # flash.now error
+      render :edit
+    end
+  end
+
+  def destroy
+    album = Album.find(params[:id])
+    band_id = album.band_id
+    album.destroy
+    redirect_to band_url(band_id)
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :recorded_location, :band_id)
+  end
+end
---
+++ projects/w4d4/music_app/app/controllers/tracks_controller.rb
@@ -0,0 +1,31 @@
+class TracksController < ApplicationController
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def track_params
+
+  end
+end
