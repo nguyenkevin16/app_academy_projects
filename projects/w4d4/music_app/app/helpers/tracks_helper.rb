195bf2525c4368eafaa1c41bf91c1727685a697f
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-08 15:23:15 -0800

    Add ugly_lyrics helper

--- projects/w4d4/music_app/app/helpers/tracks_helper.rb
+++ projects/w4d4/music_app/app/helpers/tracks_helper.rb
@@ -1,2 +1,11 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    split_lyrics = lyrics.split("\n")
+
+    html = ""
+    split_lyrics.each do |line|
+      html += "<pre>&#9835 #{h(line)}</pre>\n"
+    end
+    html.html_safe
+  end
 end
