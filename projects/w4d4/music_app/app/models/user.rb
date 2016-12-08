23e5bccf58ea74dec8315ffc8f247e6e86a1be6d
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-08 09:05:59 -0800

    Add methods for user model

--- projects/w4d4/music_app/app/models/user.rb
+++ projects/w4d4/music_app/app/models/user.rb
@@ -1,2 +1,48 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.new(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.create(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  attr_reader :password
 end
