5c3f9c15f1b179ba05a30bcd6b109aaa59008648
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-08 14:14:01 -0800

    Add views and controller actions for tracks

--- projects/w4d4/music_app/app/controllers/tracks_controller.rb
+++ projects/w4d4/music_app/app/controllers/tracks_controller.rb
@@ -1,31 +1,50 @@
 class TracksController < ApplicationController
   def show
-
+    @track = Track.find(params[:id])
+    render :show
   end
 
   def new
-
+    @albums = Album.all
+    @track = Track.new
+    render :new
   end
 
   def create
+    @track = Track.new(track_params)
 
+    if @track.save!
+      redirect_to track_url(@track)
+    else
+      redirect_to new_track_url
+    end
   end
 
   def edit
-
+    @albums = Album.all
+    @track = Track.find(params[:id])
+    render :edit
   end
 
   def update
+    @track = Track.find(params[:id])
 
+    if @track.update_attributes(track_params)
+      redirect_to track_url(@track)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @track = Track.find(params[:id])
+    @track.destroy
+    redirect_to album_url(@track.album)
   end
 
   private
 
   def track_params
-
+    params.require(:track).permit(:name, :album_id, :track_type, :lyrics)
   end
 end
