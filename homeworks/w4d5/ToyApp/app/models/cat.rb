9e9cffae731f092f330dbb575f94a7be4cdcdad0
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-08 16:53:02 -0800

    Add toyable concern and polymorphic association between toys and cats/corgis

--- homeworks/w4d5/ToyApp/app/models/cat.rb
+++ homeworks/w4d5/ToyApp/app/models/cat.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Cat < ActiveRecord::Base
+  # has_many :toys, as: :toyable
+
+  include Toyable
 end
--- homeworks/w4d5/ToyApp/app/models/corgi.rb
+++ homeworks/w4d5/ToyApp/app/models/corgi.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: corgis
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Corgi < ActiveRecord::Base
+  # has_many :toys, as: :toyable
+
+  include Toyable
 end
