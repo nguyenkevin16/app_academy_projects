4b54c1526d4519d26c462d94ced8e3c9bd820d46
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-12 15:43:14 -0800

    Add goal creation spec to goals_spec

--- GoalApp/spec/features/goals_spec.rb
+++ GoalApp/spec/features/goals_spec.rb
@@ -3,14 +3,43 @@
 require 'spec_helper'
 require 'rails_helper'
 
-feature "the goal writing process" do
+feature "the goal index" do
   before :each do
     sign_up_as_luke_skywalker
     sign_in_as_luke_skywalker
   end
 
   scenario "has a goal index page" do
-    expect(page).to have_content("New User")
+    visit goals_url
+    expect(page).to have_content("Goals")
   end
 
+  scenario "has a button to create goal" do
+    visit goals_url
+    expect(page).to have_button("Create goal")
+  end
+end
+
+feature "the goal creation" do
+  before :each do
+    sign_up_as_luke_skywalker
+    sign_in_as_luke_skywalker
+    visit goals_url
+    click_button "Create goal"
+  end
+
+  scenario "create goal button takes you to new goal form" do
+    expect(page).to have_content("New Goal")
+  end
+
+  scenario "shows new goal on index" do
+    fill_in "body", with: "dummy goal"
+    click_button "Submit"
+    expect(page).to have_content("dummy goal")
+  end
+
+  scenario "returns to new goal form with empty body" do
+    click_button "Submit"
+    expect(page).to have_content("New Goal")
+  end
 end
