f801df5ff775b77aad7d1fb4154243a280f519af
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-11 18:27:23 -0800

    Add reddit clone copy for w5d1 hw TDD hw

---
+++ homeworks/w5d1/RedditClone_copy/app/models/sub.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :integer          not null, primary key
+#  title        :string           not null
+#  description  :text
+#  moderator_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class Sub < ActiveRecord::Base
+
+  validates :title, :description, :moderator_id, presence:true
+
+  belongs_to :moderator,
+  foreign_key: :moderator_id,
+  class_name: :User
+
+  has_many :post_subs, inverse_of: :sub
+
+  has_many :posts,
+    through: :post_subs,
+    source: :post
+end
