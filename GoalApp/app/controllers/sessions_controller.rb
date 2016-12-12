d679e69d8cad3754acc9fbe8e7f1cff48398b58c
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-12 11:09:29 -0800

    Implement User Model/Controller and specs

---
+++ GoalApp/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(user_params[:username], user_params[:password])
+
+    if user
+      log_in(user)
+      # redirect somewhere
+    else
+      flash.now[:errors] = ["Not valid username/password"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_sessions_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
