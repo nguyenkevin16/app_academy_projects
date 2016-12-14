f7959ae51b1d2530bbd7a614fb69969db63dd0cf
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-13 22:30:23 -0800

    Implement phase two of intro js problems part 1

--- homeworks/w5d3/part_one/phase_two.js
+++ homeworks/w5d3/part_one/phase_two.js
@@ -2,8 +2,8 @@ let fizzBuzz = function fizzBuzz(array) {
   var bucket = [];
 
   for (let i = 0; i < array.length; i++) {
-    if ((array[i] % 3 === 0 || array[i] % 5 === 0)
-      && (array[i] % 3 === 0 && array[i] % 5 === 0)) {
+    if (array[i] % 3 === 0 || array[i] % 5 === 0) {
+      if (array[i] % 3 === 0 && array[i] % 5 === 0) { continue; }
 
       bucket.push(array[i]);
     }
@@ -12,4 +12,42 @@ let fizzBuzz = function fizzBuzz(array) {
   return bucket;
 };
 
-fizzBuzz([1,2,3,4,5]);
+let isPrime = function isPrime(number) {
+  if (number === 2) { return true; }
+
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) { return false; }
+  }
+
+  return true;
+};
+
+let sumOfNPrimes = (n) => {
+  let firstNPrimes = (num) => {
+    let bucket = [];
+    let i = 2;
+
+    while (bucket.length < num) {
+      if (isPrime(i)) { bucket.push(i); }
+      i++;
+    }
+
+    return bucket;
+  };
+
+  var sum = 0;
+  firstNPrimes(n).forEach((el) => { sum += el; });
+  return sum;
+};
+
+let allOrNothing = (mod, ...others) => {
+
+  for (let el of others) {
+    if (el % mod !== 0) { return false; }
+  }
+
+  return true;
+};
+
+allOrNothing(3, 9, 12, 6);
+allOrNothing(5, 1, 2, 10);
