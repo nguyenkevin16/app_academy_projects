f7e5d42cba4d0eccdf0bd9690d676e64490e1963
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-21 12:16:05 -0800

    Follow toggle debugging

--- ajax_twitter/skeleton/frontend/follow_toggle.js
+++ ajax_twitter/skeleton/frontend/follow_toggle.js
@@ -1,11 +1,69 @@
-class followToggle {
+class FollowToggle {
   constructor(el) {
     this.$el = $(el);
-    this.user_id = this.$el.attr("data-user-id");
-    this.followState = this.$el.attr("data-follow-state");
+    this.userId = this.$el.data("user-id");
+    this.followState = this.$el.data("initial-follow-state");
+
+    this.render();
+    this.$el.on('click', this.handleClick.bind(this));
   }
 
+  render() {
+    if (this.followState === "unfollowed") {
+      this.$el.html("Follow!");
+    } else {
+      this.$el.html("Unfollow!");
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    if (this.followState === "unfollowed") {
+      console.log("POST");
+      this.toggleState();
+      this.render();
+      // $.ajax({
+      //   type: "POST",
+      //   url: `/users/${this.userId}/follow`,
+      //   dataType: 'json',
+      //
+      //   success(button, toggleState, render) {
+      //     toggleState(button);
+      //     render();
+      //     console.log(this);
+      //   },
+      //
+      //   error: () => { console.log('error'); }
+      // });
+    } else {
+      console.log("DELETE");
+      this.toggleState();
+      this.render();
+      // $.ajax({
+      //   type: "DELETE",
+      //   url: `/users/${this.userId}/follow`,
+      //   dataType: 'json',
+      //
+      //   success(button, toggleState, render) {
+      //     toggleState(button);
+      //     render();
+      //     console.log(this);
+      //   },
+      //
+      //   error: () => { console.log('error'); }
+      // });
+    }
+  }
+
+  toggleState(){
+    console.log(this.followState === "followed");
+    if (this.followState === "unfollowed"){
+      this.$el.data('initial-follow-state', "followed");
+    } else {
+      this.$el.data('initial-follow-state', "unfollowed");
+    }
+  }
 }
 
-
-module.exports = followToggle;
+module.exports = FollowToggle;
--- ajax_twitter/skeleton/frontend/twitter.js
+++ ajax_twitter/skeleton/frontend/twitter.js
@@ -1,12 +1,7 @@
-const followToggle = require("./follow_toggle.js");
-
+const FollowToggle = require("./follow_toggle.js");
 
 $(() => {
-  const buttons = [];
-
   $("button.follow-toggle").each((idx, button)=>{
-    buttons.push(new followToggle(button));
+    new FollowToggle(button);
   });
-
-
 });
