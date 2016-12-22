30bfe5b7f2fa19a53c4be8d78c74f57d65f2ab8e
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-22 14:24:06 -0800

    Implement .attr() jquery method in jquery_lite

--- projects/w6d4/jquery_lite/lib/dom_node_collection.js
+++ projects/w6d4/jquery_lite/lib/dom_node_collection.js
@@ -10,6 +10,46 @@ class DOMNodeCollection {
       return this.elements[0].innerHTML;
     }
   }
+
+  empty() {
+    this.elements.forEach((el) => { el.innerHTML = ""; });
+  }
+
+  append(args) {
+    if (typeof args === 'string' || args instanceof HTMLElement) {
+      this.elements.forEach((el) => {
+        el.innerHTML += args;
+      });
+    } else {
+      args.elements.forEach((outer) => {
+        this.elements.forEach((inner) => {
+          inner.innerHTML += outer.outerHTML;
+        });
+      });
+    }
+  }
+
+  attr(attribute, value = "") {
+    if (value === "") {
+      return this.elements[0].getAttribute(attribute);
+    } else if (value === null) {
+      this.elements.forEach(el => {
+        el.removeAttribute(attribute);
+      });
+    } else {
+      this.elements.forEach((el) => {
+        el.setAttribute(attribute, value);
+      });
+    }
+  }
+
+  addClass() {
+
+  }
+
+  removeClass() {
+
+  }
 }
 
 module.exports = DOMNodeCollection;
