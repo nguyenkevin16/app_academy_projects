052d8c81df100cb856684c072632c03663938666
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-22 16:19:07 -0800

    Add ajax implementation for jquery_lite

--- projects/w6d4/jquery_lite/lib/jquery_lite.js
+++ projects/w6d4/jquery_lite/lib/jquery_lite.js
@@ -68,6 +68,64 @@
 	  return new DOMNodeCollection(nodeListArr);
 	};
 
+	window.$l.extend = function(...objs) {
+	  return Object.assign(...objs);
+	};
+
+	window.$l.ajax = function(options) {
+	  const params = window.$l.extend(ajaxDefaults, options);
+
+	  const xhr = new XMLHttpRequest();
+	  xhr.open(params.method, params.url);
+
+	  xhr.onload = function () {
+	    if (xhr.status === 200) {
+	      params.success(JSON.parse(xhr.response));
+	    } else {
+	      params.error(JSON.parse(xhr.response));
+	    }
+	  };
+
+	  xhr.send(params);
+	};
+
+	const ajaxDefaults = {
+	  method: "GET",
+	  url: "",
+	  dataType: "json",
+	  data: {},
+	  success: function() { alert("success"); },
+	  error: function() { alert("fail"); }
+	};
+
+
+	//  AJAX IMPLEMENTATION TESTING
+
+
+	// window.$l.ajax({
+	//   type: 'GET',
+	//   url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+	//   success(data) {
+	//     console.log("We have your weather!");
+	//     console.log(data);
+	//   },
+	//   error() {
+	//     console.error("An error occurred.");
+	//   },
+	// });
+	//
+	// $.ajax({
+	//   type: 'GET',
+	//   url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+	//   success(data) {
+	//     console.log("We have your weather!");
+	//     console.log(data);
+	//   },
+	//   error() {
+	//     console.error("An error occurred.");
+	//   },
+	// });
+
 
 /***/ },
 /* 1 */
