68697f954d9b75dbbb518515859df4e24a0326c9
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2016-12-22 16:33:50 -0800

    Add click event listeners for sidebar nav to update hashchange

--- projects/w6d4/mail/skeleton/bundle.js
+++ projects/w6d4/mail/skeleton/bundle.js
@@ -44,7 +44,15 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	console.log("It's working!");
+	document.addEventListener("DOMContentLoaded", (e) => {
+	  const sidebarLi = Array.from(document.querySelectorAll('.sidebar-nav li'));
+
+	  sidebarLi.forEach((li) => {
+	    li.addEventListener("click", (e2) => {
+	      window.location.hash = li.innerText.toLowerCase();
+	    });
+	  });
+	});
 
 
 /***/ }
--- projects/w6d4/mail/skeleton/main.js
+++ projects/w6d4/mail/skeleton/main.js
@@ -1,1 +1,9 @@
-console.log("It's working!");
+document.addEventListener("DOMContentLoaded", (e) => {
+  const sidebarLi = Array.from(document.querySelectorAll('.sidebar-nav li'));
+
+  sidebarLi.forEach((li) => {
+    li.addEventListener("click", (e2) => {
+      window.location.hash = li.innerText.toLowerCase();
+    });
+  });
+});
