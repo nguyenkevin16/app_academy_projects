305d3a086b8411630dbef899ba478473889bc162
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-02 11:48:34 -0800

    Implement reducers and store for todoApp

--- todoApp/frontend/todo_redux.jsx
+++ todoApp/frontend/todo_redux.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById("content");
   ReactDOM.render(<h1>Todos App</h1>, root);
+
+  // TODO: REMOVE LATER
+  window.store = store;
 });
