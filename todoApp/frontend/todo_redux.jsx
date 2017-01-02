fbe460e21d815e00e7ca9e7f09b6118604f88644
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-02 13:40:28 -0800

    Implement root and app components for todoApp

--- todoApp/frontend/todo_redux.jsx
+++ todoApp/frontend/todo_redux.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById("content");
-  ReactDOM.render(<h1>Todos App</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 
   // TODO: REMOVE LATER
   window.store = store;
