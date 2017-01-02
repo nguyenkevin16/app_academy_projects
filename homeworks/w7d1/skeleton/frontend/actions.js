bba05de571054f9ef15980f806206bce0eb45d88
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-01 19:56:56 -0800

    Implement action creator for switching currencies

---
+++ homeworks/w7d1/skeleton/frontend/actions.js
@@ -0,0 +1,9 @@
+export const SWITCH_CURRENCY = "SWITCH_CURRENCY";
+
+export const selectCurrency = (baseCurrency, rates) => ({
+  type: SWITCH_CURRENCY,
+  baseCurrency,
+  rates
+});
+
+// window.selectCurrency = selectCurrency;
