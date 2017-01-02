0a72cd76e142da4e50e5defd43e0569d0bda0fc8
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-01 20:02:31 -0800

    Implement action reducer for switching currencies

--- homeworks/w7d1/skeleton/frontend/reducer.js
+++ homeworks/w7d1/skeleton/frontend/reducer.js
@@ -1,10 +1,22 @@
+import { selectCurrency, SWITCH_CURRENCY } from './actions';
+
 const initialState = {
   baseCurrency: "Please select",
   rates: {}
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch(action.type) {
+    case SWITCH_CURRENCY:
+      return {
+        baseCurrency: action.baseCurrency,
+        rates: action.rates
+      };
+    default:
+      return state;
+  }
 };
+
+// window.reducer = reducer;
 
 export default reducer;
