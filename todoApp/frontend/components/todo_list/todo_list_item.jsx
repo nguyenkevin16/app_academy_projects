3a36525ec1245b7e7dcea82e057f60cd73985fb1
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-02 15:52:03 -0800

    refactor TodoListItem

--- todoApp/frontend/components/todo_list/todo_list_item.jsx
+++ todoApp/frontend/components/todo_list/todo_list_item.jsx
@@ -1,31 +1,15 @@
 import React from 'react';
 
-class TodoListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.todo = this.props.todo;
-    this.title = this.todo.title;
-    this.removeTodo = this.props.removeTodo;
-    this.updateTodo = this.props.updateTodo;
-
-    this.renderUpdate = this.renderUpdate.bind(this);
-  }
-
-  renderUpdate() {
-    return (this.props.todo.done === false ? "Done" : "Undo");
-  }
-
-  render() {
-    return (
-      <div>
-        <li>{ this.title }</li>
-        <button onClick={this.removeTodo(this.todo)}>Delete</button>
-        <button onClick={(this.updateTodo(this.todo))}>
-          {this.renderUpdate()}
-        </button>
-      </div>
-    );
-  }
-}
+const TodoListItem = ({todo, removeTodo, updateTodo}) => {
+  return (
+    <div>
+      <li>{ todo.title }</li>
+      <button onClick={removeTodo(todo)}>Delete</button>
+      <button onClick={(updateTodo(todo))}>
+        {todo.done === false ? "Done" : "Undo"}
+      </button>
+    </div>
+  );
+};
 
 export default TodoListItem;
