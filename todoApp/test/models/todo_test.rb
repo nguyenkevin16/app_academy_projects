938c2d526d22410c4ee0686f97784a5f7e07ec5a
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-03 10:47:52 -0800

    Fixed project naming structure

--- todoApp/test/models/todo_test.rb
+++ todoApp/test/models/todo_test.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: todos
-#
-#  id         :integer          not null, primary key
-#  title      :string           not null
-#  body       :string           not null
-#  done       :boolean          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
