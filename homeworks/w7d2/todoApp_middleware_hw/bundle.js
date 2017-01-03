e8996573942de1373d920bc207e9be993d2e7bba
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-03 05:36:37 -0800

    Implement basic logger middleware to middleware hw

--- homeworks/w7d2/todoApp_middleware_hw/bundle.js
+++ homeworks/w7d2/todoApp_middleware_hw/bundle.js
@@ -68,9 +68,22 @@
 	  var preloadedState = localStorage.state ? JSON.parse(localStorage.state) : {};
 	  var store = (0, _store2.default)(preloadedState);
 	
+	  store.dispatch = addLoggingToDispatch(store);
+	
 	  var root = document.getElementById('content');
 	  _reactDom2.default.render(_react2.default.createElement(_root2.default, { store: store }), root);
 	});
+	
+	var addLoggingToDispatch = function addLoggingToDispatch(store) {
+	  var dispatch = store.dispatch;
+	
+	  return function (action) {
+	    console.log(store.getState());
+	    console.log(action);
+	    dispatch(action);
+	    console.log(store.getState());
+	  };
+	};
 
 /***/ },
 /* 1 */
