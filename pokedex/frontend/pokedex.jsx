84a7aa42817324bcc98fe18dc78de2c6363dc08b
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-04 12:03:21 -0800

    Add frontend structure for pokedex

---
+++ pokedex/frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", ()=> {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>pokedex</h1>, rootEl);
+});
