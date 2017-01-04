9d45efebdc8c928bbebba10bd590ebec89e48e4f
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-03 17:53:23 -0800

    Implement styling CSS

--- todoApp/frontend/components/todo_list/todo_form.jsx
+++ todoApp/frontend/components/todo_list/todo_form.jsx
@@ -46,44 +46,47 @@ class TodoForm extends React.Component {
   render() {
     return (
       <form className="todo-form" onSubmit={this.handleSubmit}>
-        <label>Title:
-          <input
-            className="input"
-            ref="title"
-            value={this.state.title}
-            placeholder="buy milk"
-            onChange={this.update('title')}
-            required/>
-        </label>
-        <label>Body:
-          <textarea
-            className="input"
-            ref="body"
-            cols='20'
-            value={this.state.body}
-            rows='5'
-            placeholder="2% or whatever is on sale!"
-            onChange={this.update('body')}
-            required></textarea>
-        </label>
+        <div className="todo-form-input">
+          <label>Title:
+            <input
+              className="input"
+              ref="title"
+              value={this.state.title}
+              placeholder="buy milk"
+              onChange={this.update('title')}
+              required/>
+          </label>
+          <label>Body:
+            <textarea
+              className="input"
+              ref="body"
+              cols='20'
+              value={this.state.body}
+              rows='5'
+              placeholder="2% or whatever is on sale!"
+              onChange={this.update('body')}
+              required></textarea>
+          </label>
 
-        <label>Tags:
-          <ul>
-            {
-              this.state.tag_names.map((tag, i) => (
-                <li key={i}>{ tag }</li>
-              ))
-            }
-          </ul>
+          <label>Tags:
+            <ul className="tag-list">
+              {
+                this.state.tag_names.map((tag, i) => (
+                  <li key={i}>tag: { tag }</li>
+                ))
+              }
+            </ul>
 
-          <input type="text"
-            value={this.state.tagInput}
-            onChange={this.updateTag} />
-          <button type="button"
-            onClick={this.handleTag}>
-            Create Tag!
-          </button>
-        </label>
+            <input type="text"
+              value={this.state.tagInput}
+              onChange={this.updateTag} />
+            <button type="button"
+              className="create-button"
+              onClick={this.handleTag}>
+              Create Tag!
+            </button>
+          </label>
+        </div>
 
         <button className="create-button">Create Todo!</button>
 
