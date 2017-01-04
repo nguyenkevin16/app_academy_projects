613454a5fb4a763518d74b57402fe5f9ffc9292e
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-03 18:47:42 -0800

    Implement giphy action, reducer, store, and util

--- homeworks/w7d3/giphy/giphy_search.jsx
+++ homeworks/w7d3/giphy/giphy_search.jsx
@@ -2,3 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+
+import { fetchSearchGiphys } from './actions/giphy_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  const store = configureStore();
+
+  // ReactDOM.render(<Root store={store} />, root);
+
+  window.store = store;
+  window.fetchSearchGiphys = fetchSearchGiphys;
+});
