4d742178654887a4a9d013974aa706ec26252de8
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-01-06 12:31:13 -0800

    Implement frontend auth framework i.e. session actions, reducers, and store

--- projects/w7d5/bench_bnb/frontend/bench_bnb.jsx
+++ projects/w7d5/bench_bnb/frontend/bench_bnb.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
 import { login, logout, signup } from './utils/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>hello react</h1>, root);
 
   // TODO: testing
+  window.store = store;
   window.login = login;
   window.logout = logout;
   window.signup = signup;
