7b5500b52d1de855b6a48bc04e722d15760d96c1
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-02-06 10:31:54 -0800

    Complete linked list

--- jobsearch/lrucache/skeleton/lib/p04_linked_list.rb
+++ jobsearch/lrucache/skeleton/lib/p04_linked_list.rb
@@ -11,10 +11,24 @@ class Link
   def to_s
     "#{@key}: #{@val}"
   end
+
+  def remove
+    self.prev.next = self.next
+    self.next.prev = self.prev
+    self.next = nil
+    self.prev = nil
+  end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,31 +37,57 @@ class LinkedList
   end
 
   def first
+    @head.next unless empty?
   end
 
   def last
+    @tail.prev unless empty?
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |node| return node.val if node.key == key }
+    nil
   end
 
   def include?(key)
+    any? { |node| node.key == key }
   end
 
   def insert(key, val)
+    insert_link = Link.new(key, val)
+    prev_link = @tail.prev
+
+    prev_link.next = insert_link
+    insert_link.next = @tail
+    insert_link.prev = prev_link
+    @tail.prev = insert_link
   end
 
   def remove(key)
+    node_to_remove = @head.next
+
+    until node_to_remove.key == key
+      node_to_remove = node_to_remove.next
+    end
+
+    node_to_remove.remove
   end
 
   def each
+    node_to_check = @head.next
+
+    until node_to_check == @tail
+       yield(node_to_check)
+      node_to_check = node_to_check.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
