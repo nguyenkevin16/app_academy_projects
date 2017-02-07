42a3d76c4ac2b81228dc07acf9338134e2a5a00a
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-02-06 18:16:31 -0800

    Implement heapsort

--- jobsearch/heap/skeleton/lib/heap_sort.rb
+++ jobsearch/heap/skeleton/lib/heap_sort.rb
@@ -2,5 +2,16 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    heap = BinaryMinHeap.new
+
+    each do |el|
+      heap.push(el)
+    end
+
+    length.times do |i|
+      self[i] = heap.extract
+    end
+
+    self
   end
 end
