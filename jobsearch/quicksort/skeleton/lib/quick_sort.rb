5a89582a56048c196254ece10307ea5f970745f1
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-02-07 17:41:10 -0800

    Implement recursive quicksort and partition

--- jobsearch/quicksort/skeleton/lib/quick_sort.rb
+++ jobsearch/quicksort/skeleton/lib/quick_sort.rb
@@ -3,13 +3,37 @@ class QuickSort
   # case O(n**2).
 
   # Not in-place. Uses O(n) memory.
-  def self.sort1(array)
+  def self.sort1(array, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+
+    return array if array.length < 2
+
+    pivot = array.first
+    left = array[1..-1].select { |el| prc.call(el, pivot) == -1 }
+    right = array[1..-1].select { |el| prc.call(el, pivot) != -1 }
+
+    left.my_quick_sort(&prc) + [pivot] + right.my_quick_sort(&prc)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    QuickSort.partition(array, start, length)
   end
 
   def self.partition(array, start, length, &prc)
+    pivot_idx = length / 2 + start
+    partition_idx = start
+
+    # swap first value with pivot value
+    array[pivot_idx], array[start] = array[pivot_idx], array[start]
+
+    (start + 1...start + length).each do |idx|
+      if array[idx] <= array[start]
+        array[partition_idx + 1], array[idx] = array[idx], array[partition_idx + 1]
+        partition_idx += 1
+      end
+    end
+
+    pivot_idx
   end
 end
