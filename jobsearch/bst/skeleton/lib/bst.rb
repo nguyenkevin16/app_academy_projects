d1a027a446e2c0e21441650e4ca0cca5ae5ccfab
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-02-08 17:36:48 -0800

    Implement BST max, min, insert

--- jobsearch/bst/skeleton/lib/bst.rb
+++ jobsearch/bst/skeleton/lib/bst.rb
@@ -3,21 +3,26 @@ class BSTNode
   attr_reader :value
 
   def initialize(value)
-
+    @left, @right = nil
+    @value = value
   end
 end
 
 class BinarySearchTree
   def initialize
-
+    @root = nil
   end
 
   def insert(value)
-
+    if @root
+      BinarySearchTree.insert!(@root, value)
+    else
+      @root = BSTNode.new(value)
+    end
   end
 
   def find(value)
-
+    BinarySearchTree.find!(@root, value)
   end
 
   def inorder
@@ -33,15 +38,15 @@ class BinarySearchTree
   end
 
   def height
-
+    BinarySearchTree.height!(@root)
   end
 
   def min
-
+    BinarySearchTree.min(@root)
   end
 
   def max
-
+    BinarySearchTree.max(@root)
   end
 
   def delete(value)
@@ -49,11 +54,24 @@ class BinarySearchTree
   end
 
   def self.insert!(node, value)
+    return BSTNode.new(value) unless node
 
+    if value <= node.value
+      node.left = BinarySearchTree.insert!(node.left, value)
+    else
+      node.right = BinarySearchTree.insert!(node.right, value)
+    end
+
+    node
   end
 
   def self.find!(node, value)
+    return nil unless node
+    return nil if node.right.nil? && node.left.nil?
+    return node if value == node.value
 
+    BinarySearchTree.find!(node.left, value)
+    BinarySearchTree.find!(node.right, value)
   end
 
   def self.preorder!(node)
@@ -69,15 +87,28 @@ class BinarySearchTree
   end
 
   def self.height!(node)
+    return -1 unless node
+    return 0 if node.right.nil? && node.left.nil?
+    return 1 if node.right || node.left
 
+    left_depth, right_depth = 0
+    left_depth + BinarySearchTree.height!(node.left)
+    right_depth + BinarySearchTree.height!(node.right)
+
+    p right_depth
+    p left_depth
+
+    right_depth > left_depth ? right_depth : left_depth
   end
 
   def self.max(node)
-
+    return node unless node.right
+    BinarySearchTree.max(node.right)
   end
 
   def self.min(node)
-
+    return node unless node.left
+    BinarySearchTree.min(node.left)
   end
 
   def self.delete_min!(node)
