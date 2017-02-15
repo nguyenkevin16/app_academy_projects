a3c6490826d6880a9da6b375eefcbdcdfeded656
nguyenkevin16 <nguyenkevin16@users.noreply.github.com>
2017-02-15 11:25:00 -0800

    Fix topo sort

--- jobsearch/top_sort/skeleton/lib/topological_sort.rb
+++ jobsearch/top_sort/skeleton/lib/topological_sort.rb
@@ -3,26 +3,24 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
-  in_edge_counts = {}
-  queue = []
-  sorted_vertices = []
+  counts, queue, sorted = {}, [], []
 
-  vertices.each do |v|
-    in_edge_counts[v] = v.in_edges.count
-    queue << v if v.in_edges.empty?
+  vertices.each do |vertex|
+    counts[vertex] = vertex.in_edges.count
+    queue << vertex if vertex.in_edges.empty?
   end
 
   until queue.empty?
     vertex = queue.shift
-    sorted_vertices << vertex
+    sorted << vertex
 
-    vertex.out_edges.each do |e|
-      to_vertex = e.to_vertex
+    vertex.out_edges.each do |edge|
+      to_vertex = edge.to_vertex
 
-      in_edge_counts[to_vertex] -= 1
-      queue << to_vertex if in_edge_counts[to_vertex] == 0
+      counts[to_vertex] -= 1
+      queue << to_vertex if counts[to_vertex] == 0
     end
   end
 
-  sorted_vertices
+  sorted
 end
